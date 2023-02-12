64642acebf896cf82a626b6e2cc4a35f855aa545
amy <amy>
2023-02-12 16:20:05 +0800

    deploy

--- assets/IndexView-4bd37fa0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-eeada766.js";const o={},s={class:"text-center"};function n(r,a,_,p,d,f){return t(),c("h2",s,"首頁")}const l=e(o,[["render",n]]);export{l as default};
